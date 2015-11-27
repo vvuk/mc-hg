vvuk/gecko-dev

Bug 1184283 - Move vsync notification/distribution to nsIWidget and nsBaseWidget
All incoming vsync notifications (from hardware or other sources) come
into a widget, which will then distribute it to its child widgets,
its refresh drivers, compositors, etc.

This gives us a single place to override the current vsync source,
so that we can handle per-monitor vsync, VR HMDs, etc.

Child processes have their PuppetWidgets listen to vsync directly
for the appropriate source (instead of going through their
parent cross-process widget).

widget/gonk/nsScreenManagerGonk.cpp
@@ -676,11 +676,11 @@ nsScreenManagerGonk::VsyncControl(bool aEnabled)
     }
 
     MOZ_ASSERT(NS_IsMainThread());
-    VsyncSource::Display &display = gfxPlatform::GetPlatform()->GetHardwareVsync()->GetGlobalDisplay();
+    nsRefPtr<VsyncDisplay> display = gfxPlatform::GetPlatform()->GetHardwareVsync()->GetGlobalDisplay();
     if (aEnabled) {
-        display.EnableVsync();
+        display->EnableVsync();
     } else {
-        display.DisableVsync();
+        display->DisableVsync();
     }
 }
 

vvuk/mc-hg

widget/gonk/nsScreenManagerGonk.cpp
@@ -24,7 +24,7 @@
 #include "nsScreenManagerGonk.h"
 #include "nsThreadUtils.h"
 #include "HwcComposer2D.h"
-#include "VsyncSource.h"
+#include "gfxVsync.h"
 #include "nsWindow.h"
 #include "mozilla/layers/CompositorParent.h"
 #include "mozilla/Services.h"
@@ -694,11 +694,11 @@ nsScreenManagerGonk::VsyncControl(bool aEnabled)
     }
 
     MOZ_ASSERT(NS_IsMainThread());
-    VsyncSource::Display &display = gfxPlatform::GetPlatform()->GetHardwareVsync()->GetGlobalDisplay();
+    RefPtr<VsyncSource> display = gfxPlatform::GetPlatform()->GetHardwareVsync()->GetGlobalDisplaySource();
     if (aEnabled) {
-        display.EnableVsync();
+        display->EnableVsync();
     } else {
-        display.DisableVsync();
+        display->DisableVsync();
     }
 }
 