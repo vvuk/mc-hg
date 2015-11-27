vvuk/gecko-dev

Bug 1184283 - Have HMDs create their own VsyncDisplay, and have widget keep track of currently attached HMD

gfx/vr/gfxVR.h
@@ -8,6 +8,7 @@
 
 #include "nsTArray.h"
 #include "nsIScreen.h"
+#include "nsID.h"
 #include "nsString.h"
 #include "nsCOMPtr.h"
 #include "mozilla/nsRefPtr.h"
@@ -176,6 +177,7 @@ public:
   VRHMDType GetType() const { return mType; }
   uint32_t GetDeviceIndex() const { return mDeviceIndex; }
   const nsCString& GetDeviceName() const { return mDeviceName; }
+  const nsID& GetDisplayID() const { return mDisplayID; }
 
   virtual const VRFieldOfView& GetRecommendedEyeFOV(uint32_t whichEye) { return mRecommendedEyeFOV[whichEye]; }
   virtual const VRFieldOfView& GetMaximumEyeFOV(uint32_t whichEye) { return mMaximumEyeFOV[whichEye]; }
@@ -202,7 +204,6 @@ public:
 
   virtual void ZeroSensor() = 0;
 
-
   // if rendering is offloaded
   virtual VRHMDRenderingSupport *GetRenderingSupport() { return nullptr; }
 
@@ -227,6 +228,7 @@ protected:
   VRHMDConfiguration mConfiguration;
   uint32_t mDeviceIndex;
   nsCString mDeviceName;
+  nsID mDisplayID;
 
   VRFieldOfView mEyeFOV[NumEyes];
   IntSize mEyeResolution;

gfx/vr/gfxVROculus.cpp
@@ -8,20 +8,21 @@
 #include "prlink.h"
 #include "prmem.h"
 #include "prenv.h"
+#include "gfxPlatform.h"
 #include "gfxPrefs.h"
+#include "gfxUtils.h"
 #include "nsString.h"
+#include "SoftwareVsyncSource.h"
 #include "mozilla/Preferences.h"
-
 #include "mozilla/gfx/Quaternion.h"
+#include "nsServiceManagerUtils.h"
+#include "nsIScreenManager.h"
 
 #ifdef XP_WIN
 #include "../layers/d3d11/CompositorD3D11.h"
 #endif
 
 #include "gfxVROculus.h"
-
-#include "nsServiceManagerUtils.h"
-#include "nsIScreenManager.h"
 
 #ifndef M_PI
 # define M_PI 3.14159265358979323846
@@ -256,6 +257,8 @@ HMDInfoOculus::HMDInfoOculus(ovrHmd aHMD)
   , mStartCount(0)
   , mPerfHudMode(0)
 {
+  MOZ_ASSERT(XRE_IsParentProcess(),
+             "Can only create HMDInfoOculus in XRE parent process!");
   MOZ_ASSERT(sizeof(HMDInfoOculus::DistortionVertex) == sizeof(VRDistortionVertex),
              "HMDInfoOculus::DistortionVertex must match the size of VRDistortionVertex");
 
@@ -279,6 +282,11 @@ HMDInfoOculus::HMDInfoOculus(ovrHmd aHMD)
 
   SetFOV(mRecommendedEyeFOV[Eye_Left], mRecommendedEyeFOV[Eye_Right], 0.01, 10000.0);
 
+  // create a vsync source for this display and register it
+  gfxUtils::GenerateUUID(&mDisplayID);
+  mVsyncDisplay = new SoftwareDisplay(mDisplayID, 1000.0 / mDesc.DisplayRefreshRate);
+  gfxPlatform::GetPlatform()->GetHardwareVsync()->RegisterDisplay(mVsyncDisplay);
+
 #if 1
   int32_t xcoord = 0;
   if (getenv("FAKE_OCULUS_SCREEN")) {
@@ -300,6 +308,12 @@ HMDInfoOculus::HMDInfoOculus(ovrHmd aHMD)
 void
 HMDInfoOculus::Destroy()
 {
+  if (mVsyncDisplay) {
+    gfxPlatform::GetPlatform()->GetHardwareVsync()->UnregisterDisplay(mDisplayID);
+    mVsyncDisplay->Shutdown();
+    mVsyncDisplay = nullptr;
+  }
+
   if (mHMD) {
     ovr_Destroy(mHMD);
     mHMD = nullptr;

widget/nsBaseWidget.h
@@ -345,6 +345,9 @@ public:
 
   virtual void StartAsyncScrollbarDrag(const AsyncDragMetrics& aDragMetrics) override;
 
+  void SetAttachedHMD(mozilla::gfx::VRHMDInfo* aHMD) override;
+  mozilla::gfx::VRHMDInfo* GetAttachedHMD() override;
+
   /**
    * Use this when GetLayerManager() returns a BasicLayerManager
    * (nsBaseWidget::GetLayerManager() does). This sets up the widget's
@@ -548,6 +551,8 @@ protected:
   bool              mUpdateCursor;
   bool              mUseAttachedEvents;
   bool              mIMEHasFocus;
+
+  nsRefPtr<mozilla::gfx::VRHMDInfo> mHMD;
 
   static nsIRollupListener* gRollupListener;
 

vvuk/mc-hg

gfx/vr/gfxVR.h
@@ -8,6 +8,7 @@
 
 #include "nsTArray.h"
 #include "nsIScreen.h"
+#include "nsID.h"
 #include "nsString.h"
 #include "nsCOMPtr.h"
 #include "mozilla/RefPtr.h"
@@ -176,6 +177,7 @@ public:
   VRHMDType GetType() const { return mType; }
   uint32_t GetDeviceIndex() const { return mDeviceIndex; }
   const nsCString& GetDeviceName() const { return mDeviceName; }
+  const nsID& GetVsyncSourceID() const { return mVsyncSourceID; }
 
   virtual const VRFieldOfView& GetRecommendedEyeFOV(uint32_t whichEye) { return mRecommendedEyeFOV[whichEye]; }
   virtual const VRFieldOfView& GetMaximumEyeFOV(uint32_t whichEye) { return mMaximumEyeFOV[whichEye]; }
@@ -202,7 +204,6 @@ public:
 
   virtual void ZeroSensor() = 0;
 
-
   // if rendering is offloaded
   virtual VRHMDRenderingSupport *GetRenderingSupport() { return nullptr; }
 
@@ -227,6 +228,7 @@ protected:
   VRHMDConfiguration mConfiguration;
   uint32_t mDeviceIndex;
   nsCString mDeviceName;
+  nsID mVsyncSourceID;
 
   VRFieldOfView mEyeFOV[NumEyes];
   IntSize mEyeResolution;

gfx/vr/gfxVROculus.cpp
@@ -8,20 +8,21 @@
 #include "prlink.h"
 #include "prmem.h"
 #include "prenv.h"
+#include "gfxPlatform.h"
 #include "gfxPrefs.h"
+#include "gfxUtils.h"
 #include "nsString.h"
+#include "gfxVsync.h"
 #include "mozilla/Preferences.h"
-
 #include "mozilla/gfx/Quaternion.h"
+#include "nsServiceManagerUtils.h"
+#include "nsIScreenManager.h"
 
 #ifdef XP_WIN
 #include "../layers/d3d11/CompositorD3D11.h"
 #endif
 
 #include "gfxVROculus.h"
-
-#include "nsServiceManagerUtils.h"
-#include "nsIScreenManager.h"
 
 #ifndef M_PI
 # define M_PI 3.14159265358979323846
@@ -256,6 +257,8 @@ HMDInfoOculus::HMDInfoOculus(ovrHmd aHMD)
   , mStartCount(0)
   , mPerfHudMode(0)
 {
+  MOZ_ASSERT(XRE_IsParentProcess(),
+             "Can only create HMDInfoOculus in XRE parent process!");
   MOZ_ASSERT(sizeof(HMDInfoOculus::DistortionVertex) == sizeof(VRDistortionVertex),
              "HMDInfoOculus::DistortionVertex must match the size of VRDistortionVertex");
 
@@ -279,6 +282,11 @@ HMDInfoOculus::HMDInfoOculus(ovrHmd aHMD)
 
   SetFOV(mRecommendedEyeFOV[Eye_Left], mRecommendedEyeFOV[Eye_Right], 0.01, 10000.0);
 
+  // create a vsync source for this display and register it
+  gfxUtils::GenerateUUID(&mVsyncSourceID);
+  mVsyncSource = new SoftwareVsyncSource(mVsyncSourceID, 1000.0 / mDesc.DisplayRefreshRate);
+  gfxPlatform::GetPlatform()->GetHardwareVsync()->RegisterSource(mVsyncSource);
+
 #if 1
   int32_t xcoord = 0;
   if (getenv("FAKE_OCULUS_SCREEN")) {
@@ -300,6 +308,12 @@ HMDInfoOculus::HMDInfoOculus(ovrHmd aHMD)
 void
 HMDInfoOculus::Destroy()
 {
+  if (mVsyncSource) {
+    gfxPlatform::GetPlatform()->GetHardwareVsync()->UnregisterSource(mVsyncSourceID);
+    mVsyncSource->Shutdown();
+    mVsyncSource = nullptr;
+  }
+
   if (mHMD) {
     ovr_Destroy(mHMD);
     mHMD = nullptr;

widget/nsBaseWidget.h
@@ -349,6 +349,9 @@ public:
 
   virtual void StartAsyncScrollbarDrag(const AsyncDragMetrics& aDragMetrics) override;
 
+  void SetAttachedHMD(mozilla::gfx::VRHMDInfo* aHMD) override;
+  mozilla::gfx::VRHMDInfo* GetAttachedHMD() override;
+
   /**
    * Use this when GetLayerManager() returns a BasicLayerManager
    * (nsBaseWidget::GetLayerManager() does). This sets up the widget's
@@ -553,6 +556,8 @@ protected:
   bool              mUpdateCursor;
   bool              mUseAttachedEvents;
   bool              mIMEHasFocus;
+
+  RefPtr<mozilla::gfx::VRHMDInfo> mHMD;
 
   static nsIRollupListener* gRollupListener;
 